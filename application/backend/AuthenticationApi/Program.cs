f76f43aa83717f61d9fbda948fb73dc881a00068
ZASERZAFEAR\Ratchanon <naserdoctor@gmail.com>
2023-11-19 20:54:37 +0700

    Draft Microservices

---
+++ application/backend/AuthenticationApi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthenticationApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ application/backend/DepositApi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DepositApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
