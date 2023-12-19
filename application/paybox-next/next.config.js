d37bf503c6d7fad705b03a68587cdb71785bf710
Jui Sarit <splewma2008@hotmail.com>
2023-12-19 22:28:45 +0700

    add: BASE_PATH env

--- application/paybox-next/next.config.js
+++ application/paybox-next/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: 'standalone'
+    output: 'standalone',
+    basePath: process.env.BASE_PATH ?? ''
 }
 
 module.exports = nextConfig
